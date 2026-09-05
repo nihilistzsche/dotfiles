e9f5eb981442518ba8b59d37bf20e860cdb9443d
Nihilistzsche <nihilistzsche@gmail.com>
2026-09-05 16:50:28 -0700

    Revert "Fish out."
    This reverts commit b6cae60177f01945ff1a5d782f5bae82edc75a05.

---
+++ dot_config/fish/functions/down-line-or-beginning-search.fish
@@ -0,0 +1,26 @@
+function down-line-or-beginning-search -d "Search back or move cursor up 1 line"
+    # If we are already in search mode, continue
+    if commandline --search-mode
+        commandline -f history-prefix-search-forward
+        return
+    end
+
+    # If we are navigating the pager, then up always navigates
+    if commandline --paging-mode
+        commandline -f down-line
+        return
+    end
+
+    # We are not already in search mode.
+    # If we are on the top line, start search mode,
+    # otherwise move up
+    set -l lineno (commandline -L)
+
+    switch $lineno
+        case 1
+            commandline -f history-prefix-search-forward
+
+        case '*'
+            commandline -f down-line
+    end
+end
