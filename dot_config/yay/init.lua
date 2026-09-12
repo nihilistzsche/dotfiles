7b912f8d692b7fe20f9dab5fbfe46dc5bcb6fabf
Nihilistzsche <nihilistzsche@gmail.com>
2026-09-12 16:44:35 -0700

    Fix makepkg command.

--- dot_config/yay/init.lua
+++ dot_config/yay/init.lua
@@ -1,20 +1,20 @@
 yay.opt.answer_diff = "ALL"
-yay.opt.makepkg_bin = "makepkg-bin"
+yay.opt.makepkg_bin = "makepkg-nc"
 require("hooks.maintainer_change")
 require("hooks.recently_modified")
 
 -- >>> aurscan begin (managed block; do not edit by hand)
 -- aurscan: scan each AUR package (PKGBUILD + downloaded sources) before install.
 yay.create_autocmd("AURPostDownload", {
-	desc = "aurscan pre-build malware scan",
-	callback = function(event)
-		local function shq(s)
-			return "'" .. tostring(s):gsub("'", "'\\''") .. "'"
-		end
-		local cmd = "/usr/bin/aurscan" .. " --prebuild " .. shq(event.data.dir)
-		if os.execute(cmd) ~= 0 then
-			yay.abort(event.match .. ": blocked by aurscan")
-		end
-	end,
+    desc = "aurscan pre-build malware scan",
+    callback = function(event)
+        local function shq(s)
+            return "'" .. tostring(s):gsub("'", "'\\''") .. "'"
+        end
+        local cmd = "/usr/bin/aurscan" .. " --prebuild " .. shq(event.data.dir)
+        if os.execute(cmd) ~= 0 then
+            yay.abort(event.match .. ": blocked by aurscan")
+        end
+    end,
 })
 -- <<< aurscan end
